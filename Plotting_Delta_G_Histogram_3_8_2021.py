d7e4cf24bc15f4609a617aee96dd424519e04695
Hagandasj <haganjamest@gmail.com>
2021-03-08 11:35:11 -0500

    Brought the class up to working condition. Can extract current and votlage data.

--- Plotting_Delta_G_Histogram_3_8_2021.py
+++ Plotting_Delta_G_Histogram_3_8_2021.py
@@ -10,7 +10,7 @@ Created on Mon Mar  8 10:01:20 2021
 
 """ ----- Variables to Change ----- """
 
-files = ["JH_L01_2_15_2021_pH7_1MKCl_Azo_2_UV365nm_1_White_4_p200mV_blank_1737"]
+files = "JH_L01_2_15_2021_pH7_1MKCl_Azo_2_UV365nm_1_White_4_p200mV_blank_1737"
 
 
 # MAKE A FOLDER TO SAVE THE EVENT ANALYSIS INFO INTO!!! cope path and enter below with "/analysis_parameters" added to the end, or a more specific title if desired
@@ -28,29 +28,28 @@ start_time = 165
 end_time = 370 # "Whole_Run" #"Plot_the_whole_thing" <<< Uncomment this to run the whole baseline
 
 
-# Set Y-Min
-set_ylim = 0 # 0 = No; 1 = Yes
-ymin = 3000 # 750
-ymax = 5000
-
-
 #%%
 
 """ ----- Import Section ----- """
 
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import math
 
+#%%
 
+""" ----- Class and Function Section ----- """
 
 class DeltaGHistogram():
+    """ This class will be used to extract data from bin files and create
+    delta G histogram. """
     
-    import matplotlib.pyplot as plt
-    import os
-    
-    def __init__(self, start_t, end_t, x_lim, y_lim, appl_voltage, col_current, file_path, make_folder):
-        self.starT_t = start_t
+    def __init__(self, data_file, acq_rate, start_t, end_t, appl_voltage, col_current, file_path, make_folder):
+        self.data_file = data_file
+        self.acq_rate = acq_rate
+        self.star_t = start_t
         self.end_t = end_t
-        self.x_lim = x_lim
-        self.y_lim = y_lim
         self.appl_voltage = 0
         self.col_current = []
         self.file_path = file_path
@@ -58,20 +57,41 @@ class DeltaGHistogram():
         
     def plotHistogram(self):
         # Opening the data from bin files
-        with open(os.path.join(path, analysis_Title + '.bin')) as f: # assign working path to location of .bin files
+        with open(os.path.join(self.file_path, f"{self.data_file}.bin")) as working_file: # assign working path to location of .bin files
 
-        # Generating a 
-        try:
-            os.mkdir(os.path.join(path, folder[0]))
-        except OSError as error:
-            print(error)
-        
-        
-        
-        
-        
+            # Generating a 
+            try:
+                os.mkdir(os.path.join(self.file_path, self.make_folder))
+            except OSError as error:
+                print(error)
+                
+            raw_data = np.fromfile(working_file, dtype = np.dtype('>d')) # use numpy module to assign current trace data to [raw_data]
+            
+            working_file.close()
+            
+        baseline_current = raw_data[0::2] 
         
         
 #%%
+if set_ylim == 0:
+    st = 1
+    et = math.floor(len(baseline_current) / frequency)
+    
+if (type(end_time) is str) == True:
+    elapsed_time = "Whole_Run"
+    
+elif end_Time - start_Time < 1:
+    elapsed_time = "{:.0f}_m" .format((end_Time - start_Time) * 1000)
+    
+else:
+    elapsed_time = "{}" .format(end_Time - start_Time)
 
-plot_data = DeltaGHistogram()
+
+# File Name
+file_Name = "{}_{}s" .format(files[0], elapsed_time)
+
+
+
+#%%
+
+plot_data = DeltaGHistogram(data_file = files, acq_rate = , start_t = st, end_t = et, x_lim = , )
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-# This python script will convert current values from bin files to delta G data and create a histogram of the values.
+# Description
+
+This python script will convert current values from bin files to delta G data and create a histogram of the values.
+
+## Notes on Code
+
+This is not a finished product. It will not produe any results. Hoping to finish this by Friday (3/8/2021).
