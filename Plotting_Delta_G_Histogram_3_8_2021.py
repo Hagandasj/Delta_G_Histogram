d90a07a14a2df927248e561095c99bdcd6b09ead
Hagandasj <haganjamest@gmail.com>
2021-03-15 13:56:02 -0400

    Removed useless variabled form script.

--- Plotting_Delta_G_Histogram_3_8_2021.py
+++ Plotting_Delta_G_Histogram_3_8_2021.py
@@ -25,14 +25,10 @@ acquisition_rate = 100000
 
 # Times stamp for data to run
 start_time = 1
-end_time = "Whole_Run" # Inputting a string will plot the whole dataset
+end_time = 2 # Inputting a string will plot the whole dataset
 
-# Standardize Data: Sets Mean = 0
-standardize_data = True # False == No; True == Yes
 
-# Set X-Axis Limits
-set_x_axis = True # False == No; True == Yes
-# xlim_values_KDE = [-5, 5]
+
 
 #%%
 
@@ -48,32 +44,36 @@ from sklearn import preprocessing
 import seaborn as sns
 
 # Setting styles for seaborn plots
-sns.set_style(style = "darkgrid")
+# sns.set_style(style = "darkgrid")
 sns.set(font_scale = 2, font = "Arial")
 
 # Setting timer for script
 run_time = time.time()
 
 # Setting folder name to be generated
-folder_to_add = ["Conductance_KDE_Plots"]
+folder_to_add = ["Conductance_KDE_Plots", "Event_Plots"]
 
 #%%
 
 """ ----- Class and Function Section ----- """
 
 class PlotAnalyzeBaseline():
-    """ This class will be used to extract data from bin files and create
-    delta G histogram. """
-    def __init__(self, data_file, acq_rate, file_path, make_folder = "Plots_Folder", standardize = True, start_t = 1, end_t = "Whole_Run", set_xlim = False): # , xlim_val = [-4, 4]):
+    """ This class will be used to extract data from bin files and create different plots to 
+    display and analyze the data collected. """
+    
+    def __init__(self, data_file, acq_rate, file_path, start_t = 1, end_t = "Whole_Run"): # , xlim_val = [-4, 4]):
+        """ data_file - Name of file to be opened
+            acq_rate - Aqcuisition rate for the data collected
+            standardize - Tells the program to standardize the dataset
+            start_t - Time in the dataset to start at (in seconds)
+            end_t - Time in the dataset to end (in seconds). If a string is used it will collect all data to the end
+            set_"""
         # Variables provided by user
         self.data_file = data_file
         self.acq_rate = acq_rate
-        self.star_t = start_t
+        self.start_t = start_t
         self.end_t = end_t
         self.file_path = file_path
-        self.make_folder = make_folder
-        self.standardize = standardize
-        self.set_xlim = set_xlim
         # self.xlim_val = xlim_val
         
         # Variables extracted from data
@@ -89,11 +89,7 @@ class PlotAnalyzeBaseline():
         # Opening the data from bin files
         with open(os.path.join(self.file_path, f"{self.data_file}.bin")) as working_file: # assign working path to location of .bin files
 
-            # Generating a folder where plots will be saved
-            try:
-                os.mkdir(os.path.join(self.file_path, self.make_folder))
-            except OSError as error:
-                print(error)
+            
                 
             # Extracting data as a double for most accuracy in final values
             self.ext_data = np.fromfile(working_file, dtype = np.dtype('>d')) 
@@ -110,7 +106,7 @@ class PlotAnalyzeBaseline():
         elif (type(self.end_t) is str) == True:
             end_time = math.floor(len(self.ext_data[0::2]) / self.acq_rate)
             
-        actual_start = math.floor(self.star_t * self.acq_rate) # formats start time to acquisition rate
+        actual_start = math.floor(self.start_t * self.acq_rate) # formats start time to acquisition rate
         actual_end = math.floor(end_time * self.acq_rate) #  # formats end time to acquisition rate
     
         self.working_baseline, self.ext_data = self.ext_data[0::2][actual_start:actual_end:], []
@@ -126,14 +122,17 @@ class PlotAnalyzeBaseline():
         
     def standardizeOutput(self, dataset):
         """ StandardizeOutput takes a 1D array of data and standardizes it by converting Mean == 0, but
-        it does not effect that standard deviation"""
+        it does not effect that standard deviation
+                dataset - 1D array that will be standardized. """
         return (preprocessing.scale(dataset, with_std = False))
         
         
         
     def plotHistogram(self, xmin = None, xmax = None):
         """ Function that plots a Kernel Density Estimator plot for the extracted data at a specific
-        time scale specified in the when initializing the class. """
+        time scale specified in the when initializing the class. 
+                xmin - (Optional) Sets the minimum x value displayed for the x axis.
+                xmax - (Optional) Sets the maxinum x value displayed for the x axis."""
         # Extracts all data from bin file
         self.extractBinData()
         
@@ -147,53 +146,43 @@ class PlotAnalyzeBaseline():
         delta_g_values = np.divide(self.working_baseline, self.appl_voltage)
         
         # Plotting for standardized data
-        if self.standardize == True:
-            # Standardizing dataset
-            scaled_data, delta_g_values = self.standardizeOutput(delta_g_values), []
-            print(f"Starting to Plot Data: {time.time() - run_time}")
-            fig, ax = plt.subplots(figsize = (9,8))
+        scaled_data, delta_g_values = self.standardizeOutput(delta_g_values), []
+        print(f"Starting to Plot Data: {time.time() - run_time}")
+        fig, ax = plt.subplots(figsize = (9,8))
             
-            # SciPy Method
-            # x_grid = np.linspace(-4.5, 4.5, 1000)
-            # kde = gaussian_kde(scaled_data, bw_method = 0.2 / scaled_data.std(ddof = 1))
-            # plt.plot(kde.evaluate(x_grid))
+        # SciPy Method
+        # x_grid = np.linspace(-4.5, 4.5, 1000)
+        # kde = gaussian_kde(scaled_data, bw_method = 0.2 / scaled_data.std(ddof = 1))
+        # plt.plot(kde.evaluate(x_grid))
             
-            # SciKit Learn Method
-            # kde = KernelDensity(kernel = "gaussian", bandwidth = 0.2).fit(scaled_data)
+        # SciKit Learn Method
+        # kde = KernelDensity(kernel = "gaussian", bandwidth = 0.2).fit(scaled_data)
             
-            # Seaborn method
-            sns.kdeplot(scaled_data, fill = True, palette = "crest", alpha = 0.8, linewidth = 0)
-            ax.set(xlabel = "Standardized Conductance")
+        # Seaborn method
+        sns.kdeplot(scaled_data, fill = True, palette = "crest", alpha = 0.8, linewidth = 0)
+        ax.set(xlabel = "Standardized Conductance")
             
-            # Setting limmits for x axis
-            ax.set_xlim(xmin, xmax)
-        
-        # Plotting for un-standardized data
-        elif self.standardize == False:
-            print(f"Starting to Plot Data: {time.time() - run_time}")
-            
-            fig, ax = plt.subplots(figsize = (10,9))
-            sns.kdeplot(delta_g_values, fill = True, palette = "crest", alpha = 0.8, linewidth = 0)
-            ax.set(xlabel = "Standardized Conductance")
-            
-            # Setting limmits for x axis
-            ax.set_xlim(xmin, xmax)
+        # Setting limmits for x axis
+        ax.set_xlim(xmin, xmax)
         
         # Saving the file section
         if (type(self.end_t) is str) == True:
             elapsed_time = "Whole_Run"
-        elif self.end_t - self.star_t < 1:
-            elapsed_time = "{:.0f}_m" .format((self.end_t - self.star_t) * 1000)
+        elif self.end_t - self.start_t < 1:
+            elapsed_time = "{:.0f}_m" .format((self.end_t - self.start_t) * 1000)
         else:
-            elapsed_time = "{}" .format(self.end_t - self.star_t)
+            elapsed_time = "{}" .format(self.end_t - self.start_t)
             
-        if standardize_data == True:
-            standard = "Standardized"
-            file_Name = "{}_{}s_{}" .format(self.data_file, elapsed_time, standard)
-        else:
-            file_Name = "{}_{}s" .format(self.data_file, elapsed_time)
+        standard = "Standardized"
+        file_Name = "{}_{}s_{}" .format(self.data_file, elapsed_time, standard)
+            
+        # Generating a folder where plots will be saved
+        try:
+            os.mkdir(os.path.join(self.file_path, "Conductance_KDE_Plots"))
+        except OSError as error:
+            print(error)
         
-        plt.savefig(os.path.join(self.file_path, self.make_folder) + "\\" + file_Name + ".png", dpi = 600)
+        plt.savefig(os.path.join(self.file_path, "Conductance_KDE_Plots") + "\\" + file_Name + ".png", dpi = 600)
         
         # Show and close the plots
         plt.show()
@@ -201,15 +190,14 @@ class PlotAnalyzeBaseline():
         
         
     def derivativePlot(self, xmin = None, xmax = None):
-        """ derivativePlot takes the derivative of the data and plots it. """
+        """ derivativePlot takes the derivative of the data and plots it. 
+                xmin - (Optional) Sets the minimum x value displayed for the x axis.
+                xmax - (Optional) Sets the maxinum x value displayed for the x axis. """
         # Extracts all data from bin file
         self.extractBinData()
         
         # Uses dataToUse function to truncate total dataset
         self.dataToUse()
-        
-        # Create list to show spacing for each datapoint
-        # time_spacing = np.linspace(start = 0, stop = len(self.working_baseline) / self.acq_rate, num = len(self.working_baseline))
         
         # Taking the derivative of the data provided
         der_data = np.gradient(self.working_baseline) # , (1 / self.acq_rate))
@@ -222,18 +210,73 @@ class PlotAnalyzeBaseline():
         plt.close()
         
 
-    def plotBaseline():
-        pass
+    def plotBaseline(self, ymin = None, ymax = None):
+        """ plotBaseline makes a line plot of the baseline data extracted
+        from the bin file. """
+        # Extracts all data from bin file
+        self.extractBinData()
+        
+        # Uses dataToUse function to truncate total dataset
+        self.dataToUse()
+        
+        # Creates a folder for plotting baseline
+        try:
+            os.mkdir(os.path.join(path, "Event_Plots"))
+        except OSError as error:
+            print(error)
+            
+        # Checks if a number or text file is set for the end time to know how much data to analyze.
+        if (type(self.end_t) is int) == True or (type(self.end_t) is float) == True:
+            end_time = self.end_t 
+        elif (type(self.end_t) is str) == True:
+            end_time = math.floor(len(self.ext_data[0::2]) / self.acq_rate)
+            
+        # Calculates the time values for x axis based on acquisition rate and number of datapoints
+        current_Trace_Time = np.arange((self.start_t - self.start_t),((math.floor(end_time * self.acq_rate) - math.floor(self.start_t * self.acq_rate)) / self.acq_rate), (1/self.acq_rate))
+        
+        fig, ax = plt.subplots(figsize = (25,7))  
+    
+        plt.plot(current_Trace_Time, self.working_baseline, linewidth = 2, color = 'black')
+        plt.locator_params(nbins = 7)
+        
+        ax.set_xlabel("Time (s)", size = 25, fontname = "Arial") #    Time (s)
+        ax.set_ylabel("Current(pA)", size = 25, fontname = "Arial")
+        
+        ax.set_ylim(ymin,ymax)
+        
+        # Sets parameters for axis labels
+        plt.xticks(fontsize = 22, fontname = 'Arial')
+        plt.yticks(fontsize = 22, fontname = 'Arial')
+        
+        # Sets parametesr for plot formatting
+        ax.spines['top'].set_visible(False) # Removes top and right lines of plot
+        ax.spines['right'].set_visible(False)
+        
+        ax.spines['left'].set_linewidth(3) # Makes the boarder bold
+        ax.xaxis.set_tick_params(width = 3)
+        ax.spines['bottom'].set_linewidth(3) # Makes the boarder bold
+        ax.yaxis.set_tick_params(width = 3)
+        
+        fig.tight_layout() # help keep axis titles on the figure
+        
+        plt.savefig(os.path.join(self.file_path, "Event_Plots") + "\\" + self.data_file[0] + "_" + str(self.start_t) + "_" + str(self.end_t) + ".png", dpi = 1200)
+        
+        # Displays figure and closes the figure to save memory space
+        plt.show()
+        plt.close()
 
 #%%
 
 """ ----- Calling Class and function to plot data ----- """
 
-initialize_data = PlotAnalyzeBaseline(data_file = files, acq_rate = acquisition_rate, start_t = start_time, end_t = end_time, file_path = path, make_folder = folder_to_add[0], standardize = standardize_data, set_xlim = set_x_axis) # , xlim_val = xlim_values)
+initialize_data = PlotAnalyzeBaseline(data_file = files, acq_rate = acquisition_rate, start_t = start_time, end_t = end_time, file_path = path) # , xlim_val = xlim_values)
 #%%
 current_data = initialize_data.plotHistogram(xmin = -5, xmax = 5)
 #%%
 deriv_data = initialize_data.derivativePlot(-100, 100)
+#%%
+
+initialize_data.plotBaseline()
 
 #%%
 
